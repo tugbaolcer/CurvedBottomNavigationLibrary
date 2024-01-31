3b8f9520ac2760f1b26cee969b82b6c0f9dc773f
Tuğba <tugba.olcer@portalgrup.com>
2024-01-31 16:38:13 +0300

    added: custom curved bottom navigation

--- app/src/main/java/com/tugbaolcer/curvedbottomnavigationlibrary/MainActivity.kt
+++ app/src/main/java/com/tugbaolcer/curvedbottomnavigationlibrary/MainActivity.kt
@@ -2,10 +2,33 @@ package com.tugbaolcer.curvedbottomnavigationlibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.tugbaolcer.curvedbottomnavigationlibrary.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            bottomNavigation.apply {
+                setOnNavigationItemSelectedListener (this@MainActivity)
+                inflateMenu(R.menu.bottom_nav_menu)
+            }
+        }
+
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        binding.apply {
+            bottomNavigation.changedFabIcon(bottomNavigation, item)
+        }
+        return true
+    }
+
 }
